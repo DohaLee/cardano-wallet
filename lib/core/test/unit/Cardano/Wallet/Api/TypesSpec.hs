fd71352cbb2101857520ab2f34f18b0be43a969f
Pawel Jakubas <pawel.jakubas@iohk.io>
2021-08-15 11:14:52 +0200

    correct tokenNames to comply with what cardano-api produces

--- lib/core/test/unit/Cardano/Wallet/Api/TypesSpec.hs
+++ lib/core/test/unit/Cardano/Wallet/Api/TypesSpec.hs
@@ -1948,7 +1948,7 @@ instance Arbitrary (ApiTxOut n) where
         <$> ((, Proxy @n) <$> arbitrary)
         <*> arbitrary
         <*> arbitrary
-        <*> arbitrary
+        <*> (ApiT <$> genTokenMapSmallRange)
 
 instance Arbitrary ApiTxIn where
     arbitrary = ApiTxIn
